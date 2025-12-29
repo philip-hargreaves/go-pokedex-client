a6c8d5020726b770a2dd270e373b051f431bca77
Philip Hargreaves <philiphargreaves@live.co.uk>
2025-12-29 22:58:20 +0000

    Implement command system with help and exit

--- repl.go
+++ repl.go
@@ -1,8 +1,61 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+
+		commmand, exists := getCommands()[command]
+		if exists {
+			err := commmand.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Command not found")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	formattedText := strings.TrimSpace(strings.ToLower(text))
 	return strings.Fields(formattedText)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
